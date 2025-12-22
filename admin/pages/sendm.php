f90fba3111035582eae1517de768ae86b5023209
Your GitHub Username <your.github@email.com>
2025-12-22 22:45:51 +0000

    feat: modernize support UI with SVG icons and fix mobile navigation

--- admin/pages/sendm.php
+++ admin/pages/sendm.php
@@ -79,11 +79,7 @@ if (db_query($sql)) {
 
 
 
-
-include "header.php";
-
-
-    ?>
+  ?>
 
 
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
